c4702f509470214ff88006e4e91a275ea9c2eec5
Stackbit Code Editor <projects@stackbit.com>
2022-02-25 11:03:13 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -30,6 +30,7 @@ export default function HeroSection(props) {
                 'flex',
                 'flex-col',
                 'justify-center',
+                'bg-url[]'
                 mapMinHeightStyles(sectionHeight),
                 sectionStyles.margin,
                 sectionStyles.padding || 'py-12 px-4',
