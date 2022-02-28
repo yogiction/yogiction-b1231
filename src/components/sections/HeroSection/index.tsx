913ef5404d58d316a46a0d6c7af28dbcc6649333
Stackbit Code Editor <projects@stackbit.com>
2022-02-28 12:29:11 +0000

    index.tsx: changed

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -40,7 +40,7 @@ export default function HeroSection(props) {
             )}
             style={{
                 borderWidth: sectionStyles.borderWidth ? `${sectionStyles.borderWidth}px` : null,
-                'var(--image-url)': fetchedUrl
+                'var(--image-url)': bgImage,
             }}
         >
             <div className={classNames('flex', 'w-full', mapStyles({ justifyContent: sectionJustifyContent }))}>
