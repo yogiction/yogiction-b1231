213bfabf98c99cb180952675e0aca5ae352efc4a
devanshdixit <devanshdixit01@gmail.com>
2022-03-03 12:09:12 +0000

    updated hero section

--- src/components/sections/HeroSection/index.tsx
+++ src/components/sections/HeroSection/index.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 import * as React from 'react';
-import Markdown from 'markdown-to-jsx';
 import classNames from 'classnames';
-import Image from 'next/image';
 import { getComponent } from '../../components-registry';
 import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
 import { getDataAttrs } from '../../../utils/get-data-attrs';
 import { Action, Badge } from '../../atoms';
-import { url } from 'inspector';
 
 export default function HeroSection(props) {
     const cssId = props.elementId || null;
@@ -23,37 +20,39 @@ export default function HeroSection(props) {
     const logo = props.logo || '';
     const title = props.title || '';
     return (
-        <div
-            id={cssId}
-            {...getDataAttrs(props)}
-            style={{
-                borderWidth: sectionStyles.borderWidth ? `${sectionStyles.borderWidth}px` : null,
-                backgroundImage: `url('${bgImage.url}')`,
-            }}
-            className={classNames(
-                'sb-component',
-                'sb-component-section',
-                'sb-component-hero-section',
-                colors,
-                'flex',
-                'flex-col',
-                'justify-center',
-                mapMinHeightStyles(sectionHeight),
-                sectionStyles.margin,
-                'bg-no-repeat bg-center bg-cover',
-                sectionStyles.padding || 'py-12 px-4',
-                sectionStyles.borderColor,
-                sectionStyles.borderStyle ? mapStyles({ borderStyle: sectionStyles.borderStyle }) : 'border-none',
-                sectionStyles.borderRadius ? mapStyles({ borderRadius: sectionStyles.borderRadius }) : null
-            )}
-        >
-            <div>
+        <>
+            <div
+                id={cssId}
+                {...getDataAttrs(props)}
+                style={{
+                    borderWidth: sectionStyles.borderWidth ? `${sectionStyles.borderWidth}px` : null,
+                    backgroundImage: `url('${bgImage.url}')`,
+                }}
+                className={classNames(
+                    'sb-component',
+                    'sb-component-section',
+                    'sb-component-hero-section',
+                    colors,
+                    'flex',
+                    'flex-col',
+                    'justify-center',
+                    mapMinHeightStyles(sectionHeight),
+                    sectionStyles.margin,
+                    'bg-no-repeat bg-center bg-cover',
+                    'pl-[39px] md:pl-[140px] pt-[25px] md:pt-[100px] pb-[20px] md:pb-[209px] pr-1',
+                    sectionStyles.borderColor,
+                    sectionStyles.borderStyle ? mapStyles({ borderStyle: sectionStyles.borderStyle }) : 'border-none',
+                    sectionStyles.borderRadius ? mapStyles({ borderRadius: sectionStyles.borderRadius }) : null
+                )}
+            >
                 <div>
-                    <img src={logo.url} className="h-[142px] w-[284px]" alt="" />
-                    <div style={titleStyles} className=''>{title}</div>
-                </div>
-                <div>{heroActions(props)}</div>
-                {/* <div className={classNames('flex', 'w-full', mapStyles({ justifyContent: sectionJustifyContent }))}>
+                    <div>
+                        <img src={logo.url} className="h-[50px] w-[94px] md:h-[142px] md:w-[284px]" alt="" />
+                        <div style={titleStyles} className={classNames('mt-[15px]  md:mt-[85px] text-[40px] leading-[50px] md:leading-[120px] md:text-[100px]',
+                            "max-w-[300px]  md:max-w-[805px] ")}>{title}</div>
+                    </div>
+                    <div>{heroActions(props)}</div>
+                    {/* <div className={classNames('flex', 'w-full', mapStyles({ justifyContent: sectionJustifyContent }))}>
                 <div className={classNames('w-full', mapMaxWidthStyles(sectionWidth))}>
                     <div
                         className={classNames(
@@ -81,8 +80,13 @@ export default function HeroSection(props) {
                     </div>
                 </div>
             </div> */}
+                </div>
             </div>
-        </div>
+
+            <div className={classNames("block md:hidden w-full bg-[#2c5d87] py-[24px] flex justify-center")}>
+                <div className='text-[25px] leading-[25px] text-white text-center'>SIGN UP NOW</div>
+            </div>
+        </>
     );
 }
 
@@ -137,21 +141,23 @@ function heroActions(props) {
     const styles = props.styles || {};
     const actionStyle = props.styles.actions || {};
     return (
-        <div
-            className={classNames('overflow-x-hidden', {
-                'mt-8': props.title || props.subtitle || props.text || props.badge
-            })}
-        >
+        <>
             <div
-                className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', styles.actions ? mapStyles(styles.actions) : null)}
-                data-sb-field-path=".actions"
-                style={actionStyle}
+                className={classNames('overflow-x-hidden hidden md:block', {
+                    'mt-8': props.title || props.subtitle || props.text || props.badge
+                })}
             >
-                {actions.map((action, index) => (
-                    <Action key={index} {...action} className="mb-3 mx-2 lg:whitespace-nowrap" data-sb-field-path={`.${index}`} />
-                ))}
+                <div
+                    className={classNames('flex', 'flex-wrap', 'items-center', '-mx-2', styles.actions ? mapStyles(styles.actions) : null)}
+                    data-sb-field-path=".actions"
+                    style={actionStyle}
+                >
+                    {actions.map((action, index) => (
+                        <Action key={index} {...action} className="mb-3 mx-2 lg:whitespace-nowrap" data-sb-field-path={`.${index}`} />
+                    ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
