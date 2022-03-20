b802c0f400a1e60c0e622a929f8a36af4bf7f1ce
devanshdixit01 <devanshdixit01@gmail.com>
2022-03-20 17:40:41 +0000

    updated

---
+++ src/components/sections/VideoSection/index.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+export default function VedioSection() {
+    return (
+        <div>
+            <div className="vidcontainer">
+                <div className="viditem">
+                    <img src="InspiratorDifference-Thumbnail-wPlayArrow.png" alt='' />
+                </div>
+            </div>
+        </div>
+    )
+}
