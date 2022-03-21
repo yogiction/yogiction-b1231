1a4921bed1db84364cf6941498e36bf588cddef8
Stackbit Code Editor <projects@stackbit.com>
2022-03-21 19:48:04 +0000

    index.tsx: changed

--- src/components/sections/FeatureHighlightSection/index.tsx
+++ src/components/sections/FeatureHighlightSection/index.tsx
@@ -122,14 +122,7 @@ function featureHighlightBody(props) {
                     {props.title}
                 </h2>
             )}
-            {props.subtitle && (
-                <p
-                    className={classNames('text-xl', 'sm:text-2xl', styles.subtitle ? mapStyles(styles.subtitle) : null, { 'mt-4': props.title })}
-                    data-sb-field-path=".subtitle"
-                >
-                    {props.subtitle}
-                </p>
-            )}
+            
             {props.text && (
                 <Markdown
                     options={{ forceBlock: true, forceWrapper: true }}
