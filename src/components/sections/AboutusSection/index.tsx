29e0d9df56e5de39207204c9b4115eb573c1391c
Stackbit Code Editor <projects@stackbit.com>
2022-04-20 07:33:03 +0000

    index.tsx: changed

--- src/components/sections/AboutusSection/index.tsx
+++ src/components/sections/AboutusSection/index.tsx
@@ -12,7 +12,7 @@ export default function AboutusSection(props) {
     const sectionWidth = sectionStyles.width || 'wide';
     const sectionHeight = sectionStyles.height || 'auto';
     const sectionJustifyContent = sectionStyles.justifyContent || 'center';
-    const title =  || 'center';
+    const title =  props.titleText || 'center';
     return (
         <div>
             <div
