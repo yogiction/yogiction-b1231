77e350d74cc06e275be81ec76540719a86528d5c
Stackbit Code Editor <projects@stackbit.com>
2022-04-20 07:46:55 +0000

    index.tsx: changed

--- src/components/sections/TestimonialsSection/index.tsx
+++ src/components/sections/TestimonialsSection/index.tsx
@@ -13,7 +13,7 @@ export default function TestimonialsSection(props) {
     const styles = props.styles || {};
     const sectionHeight = styles.self?.height || 'auto';
     const testimonials = props.testimonials || [];
-    const testimonials = props.testimonials || [];
+    const title = props.testimonials || [];
     const [slideIndex, setSlideIndex] = React.useState(0);
     const [slideDirect, setSlideDirect] = React.useState(false);
     const nextSlide = () => {
