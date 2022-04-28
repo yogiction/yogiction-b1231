a434e915025b1df1ba388efd508fbf6cb17df431
devanshdixit01 <devanshdixit01@gmail.com>
2022-04-28 14:50:54 +0000

    updated

--- src/components/sections/FloatingButtonSection/index.tsx
+++ src/components/sections/FloatingButtonSection/index.tsx
@@ -1,13 +1,17 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
-import ChatIcon from '../../svgs/chaticons'
-import CheckIcon from '../../svgs/checkicons'
 
 export default function FloatingButtonSection(props) {
     console.log(props);
+    const icons = props.icons;
     return (
         <div className="hidden md:block fixed bottom-[2.75rem] right-[2.75rem] z-[9999]">
-            <div><ChatIcon className="" /></div>
-            <div><CheckIcon className="" /></div>
+            {icons && icons.map((icon, index) => {
+                console.log(icon)
+                return <a key={index} href={icon.link}>
+                    <img src={icon.icon.url} alt='' />
+                </a>
+            })}
         </div>
     )
 }
