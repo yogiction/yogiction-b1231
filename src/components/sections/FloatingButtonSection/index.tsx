d6b058102ac34372b7de1402ef4b57d081af15cd
Stackbit Code Editor <projects@stackbit.com>
2022-04-28 14:10:23 +0000

    index.tsx: changed

--- src/components/sections/FloatingButtonSection/index.tsx
+++ src/components/sections/FloatingButtonSection/index.tsx
@@ -3,6 +3,7 @@ import ChatIcon from '../../svgs/chaticons'
 import CheckIcon from '../../svgs/checkicons'
 
 export default function FloatingButtonSection(props) {
+    console.log(props);
     return (
         <div className="hidden md:block fixed bottom-[2.75rem] right-[2.75rem] z-[9999]">
             <div><ChatIcon className="" /></div>
